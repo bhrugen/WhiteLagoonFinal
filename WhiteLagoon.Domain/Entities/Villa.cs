7064f370e6032e7fd82c62a98fc188e8c7e25d7e
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-06-19 12:30:38 -0500

    Add Villa Model

---
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace WhiteLagoon.Domain.Entities
+{
+    public class Villa
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+
+    }
+}
