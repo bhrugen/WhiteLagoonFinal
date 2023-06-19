387d593d9f731e8ac0ee29e6a797f8701d31a5e0
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-06-19 14:33:53 -0500

    Display Data Annotation

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -15,9 +15,11 @@ namespace WhiteLagoon.Domain.Entities
         public int Id { get; set; }
         public required string Name { get; set; }
         public string? Description { get; set; }
+        [Display(Name = "Price per night")]
         public double Price { get; set; }
         public int Sqft { get; set; }
         public int Occupancy { get; set; }
+        [Display(Name = "Image Url")]
         public string? ImageUrl { get; set; }
         public DateTime? CreatedDate { get; set; }
         public DateTime? UpdatedDate { get; set; }
