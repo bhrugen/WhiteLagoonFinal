949c58440171316e0a977a4de413f7c531b031c4
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-06-19 16:12:35 -0500

    Add Villa Number Model

--- WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
+++ WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ namespace WhiteLagoon.Infrastructure.Data
         {
         }
         public DbSet<Villa> Villas { get; set; }
+        public DbSet<VillaNumber> VillaNumbers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -49,6 +50,58 @@ namespace WhiteLagoon.Infrastructure.Data
                   Price = 400,
                   Sqft = 750,
               });
+
+            modelBuilder.Entity<VillaNumber>().HasData(
+             new VillaNumber
+             {
+                 Villa_Number = 101,
+                 VillaId = 1
+             },
+             new VillaNumber
+             {
+                 Villa_Number = 102,
+                 VillaId = 1
+             },
+             new VillaNumber
+             {
+                 Villa_Number = 103,
+                 VillaId = 1
+             },
+             new VillaNumber
+             {
+                 Villa_Number = 104,
+                 VillaId = 2
+             },
+             new VillaNumber
+             {
+                 Villa_Number = 105,
+                 VillaId = 2
+             },
+             new VillaNumber
+             {
+                 Villa_Number = 106,
+                 VillaId = 2
+             },
+             new VillaNumber
+             {
+                 Villa_Number = 107,
+                 VillaId = 3
+             },
+             new VillaNumber
+             {
+                 Villa_Number = 108,
+                 VillaId = 3
+             },
+             new VillaNumber
+             {
+                 Villa_Number = 109,
+                 VillaId = 3
+             },
+             new VillaNumber
+             {
+                 Villa_Number = 110,
+                 VillaId = 3
+             });
         }
     }
 }
---
+++ WhiteLagoon.Infrastructure/Migrations/20230619211115_addVillaNumber.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WhiteLagoon.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addVillaNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VillaNumbers",
+                columns: table => new
+                {
+                    Villa_Number = table.Column<int>(type: "int", nullable: false),
+                    VillaId = table.Column<int>(type: "int", nullable: false),
+                    SpecialDetails = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VillaNumbers", x => x.Villa_Number);
+                    table.ForeignKey(
+                        name: "FK_VillaNumbers_Villas_VillaId",
+                        column: x => x.VillaId,
+                        principalTable: "Villas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "VillaNumbers",
+                columns: new[] { "Villa_Number", "SpecialDetails", "VillaId" },
+                values: new object[,]
+                {
+                    { 101, null, 1 },
+                    { 102, null, 1 },
+                    { 103, null, 1 },
+                    { 104, null, 2 },
+                    { 105, null, 2 },
+                    { 106, null, 2 },
+                    { 107, null, 3 },
+                    { 108, null, 3 },
+                    { 109, null, 3 },
+                    { 110, null, 3 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VillaNumbers_VillaId",
+                table: "VillaNumbers",
+                column: "VillaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VillaNumbers");
+        }
+    }
+}
