22c165a7cd5f56826d66533c3f98c250543a3808
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-06-19 16:21:22 -0500

    Villa Number List

--- WhiteLagoon.Web/Controllers/VillaNumberController.cs
+++ WhiteLagoon.Web/Controllers/VillaNumberController.cs
@@ -1,12 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
 
 namespace WhiteLagoon.Web.Controllers
 {
     public class VillaNumberController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        public VillaNumberController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(int villaId)
+        {
+            List<VillaNumber> villaList = _context.VillaNumbers.ToList();
+            return View(villaList);
+        }
+        public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(VillaNumber villaNumber)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.VillaNumbers.Add(villaNumber);
+                _context.SaveChanges();
+                TempData["success"] = "Villa Number Successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(villaNumber);
         }
     }
 }
