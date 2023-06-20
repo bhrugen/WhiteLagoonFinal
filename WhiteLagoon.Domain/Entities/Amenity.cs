4f88b04b767453917bd3d385d35665439dde9146
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-06-20 13:21:46 -0500

    Add Amenity To Database

---
+++ WhiteLagoon.Domain/Entities/Amenity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteLagoon.Domain.Entities
+{
+    public class Amenity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Villa")]
+        public int VillaId { get; set; }
+        //this should not be validated
+        public Villa? Villa { get; set; }
+
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
