b22fb7cc80fb85ef9833b3f0df9883ed94412bb9
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-06-20 15:15:43 -0500

    create roles

--- WhiteLagoon.Web/Controllers/AccountController.cs
+++ WhiteLagoon.Web/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Application.Common.Utility;
 using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Web.ViewModels;
 
@@ -38,7 +39,11 @@ namespace WhiteLagoon.Web.Controllers
 
         public IActionResult Register()
         {
-
+            if (!_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+            }
             RegisterVM registerVM = new();
            
             return View(registerVM);
