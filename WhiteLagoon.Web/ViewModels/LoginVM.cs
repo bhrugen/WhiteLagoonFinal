830cdacd81b7e1a19f19fbf4c7ef0b6b7b68f1c8
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-06-20 15:06:18 -0500

    Login and Register VM

---
+++ WhiteLagoon.Web/ViewModels/LoginVM.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhiteLagoon.Web.ViewModels
+{
+    public class LoginVM
+    {
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+
+        public string? ReturnUrl { get; set; }
+    }
+}
---
+++ WhiteLagoon.Web/ViewModels/RegisterVM.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace WhiteLagoon.Web.ViewModels
+{
+    public class RegisterVM
+    {
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string? Phone { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
