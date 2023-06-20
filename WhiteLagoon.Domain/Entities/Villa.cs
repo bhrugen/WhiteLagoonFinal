14298df9d0a48a2c69512c6a24b45b8f0ed86f17
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-06-20 14:12:55 -0500

    Villa Details in Home Page

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -30,5 +30,7 @@ namespace WhiteLagoon.Domain.Entities
 
         [ValidateNever]
         public IEnumerable<Amenity> VillaAmenity { get; set; }
+        [NotMapped]
+        public bool IsAvailable { get; set; } = true;
     }
 }
