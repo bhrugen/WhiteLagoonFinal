f673bd234fb02c1d5ffafabff6da8b58890c2229
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-06-20 16:38:51 -0500

    Access Denied in Action

---
+++ WhiteLagoon.Web/Controllers/BookingController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Application.Common.Utility;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly List<string> _bookedStatus = new List<string> { "Approved", "CheckedIn" };
+        private readonly IUnitOfWork _unitOfWork;
+        public BookingController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [Authorize]
+        //show if only admin were allowed then this will not work and go to access denided
+        //[Authorize(Roles =SD.Role_Admin)]
+        public IActionResult FinalizeBooking()
+        {
+            return View();
+        }
+    }
+}
