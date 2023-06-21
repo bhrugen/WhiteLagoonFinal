ad79bf6af90f3b189d3897e07b765012f5d543e0
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-06-21 13:41:48 -0500

    populate user details

--- WhiteLagoon.Web/Controllers/BookingController.cs
+++ WhiteLagoon.Web/Controllers/BookingController.cs
@@ -21,13 +21,22 @@ namespace WhiteLagoon.Web.Controllers
         //[Authorize(Roles =SD.Role_Admin)]
         public IActionResult FinalizeBooking(int villaId, DateOnly checkInDate, int nights)
         {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            ApplicationUser user = _unitOfWork.User.Get(u => u.Id == userId);
+
             Booking booking = new()
             {
                 Villa = _unitOfWork.Villa.Get(u => u.Id == villaId, includeProperties: "VillaAmenity"),
                 CheckInDate = checkInDate,
                 Nights = nights,
                 CheckOutDate = checkInDate.AddDays(nights),
+                UserId = userId,
+                Phone = user.PhoneNumber,
+                Email = user.Email,
+                Name = user.Name,
             };
+            booking.TotalCost = booking.Villa.Price * nights;
             return View(booking);
         }
     }
