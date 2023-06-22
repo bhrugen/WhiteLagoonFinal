8757002e1d927e5d8c2b145e113a73ff36c23709
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-06-22 11:58:09 -0500

    controller endpoint for chart data

--- WhiteLagoon.Web/Controllers/DashboardController.cs
+++ WhiteLagoon.Web/Controllers/DashboardController.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Application.Common.Utility;
+using WhiteLagoon.Domain.SharedModels;
 
 namespace WhiteLagoon.Web.Controllers
 {
+    [Authorize(Roles = SD.Role_Admin)]
     public class DashboardController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
@@ -14,6 +18,22 @@ namespace WhiteLagoon.Web.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> GetTotalBookingsChartData()
+        {
+            RadialBarChartVM dashboardRadialBarChartVM = await _unitOfWork.Dashboard.GetBookingsChartDataAsync();
+
+            // Retrieve your data and format it as needed
+            var data = new
+            {
+                series = dashboardRadialBarChartVM.Series, //new int[] { 30 },
+                totalCount = dashboardRadialBarChartVM.TotalCount,
+                increaseDecreaseRatio = dashboardRadialBarChartVM.IncreaseDecreaseRatio,
+                hasRatioIncreased = dashboardRadialBarChartVM.HasRatioIncreased
+            };
+
+            return Json(data);
         }
     }
 }
